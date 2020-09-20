4d3100c38d35cfc8ddd70f998b229f9bcf8e0c50
Phan Vu Long <vulongphan@Phans-MacBook-Air.local>
2020-09-20 15:05:28 +0400

    fixing play()

--- controllers/action.js
+++ controllers/action.js
@@ -258,7 +258,7 @@ getNowPlaying = (req, res) => {
                     var time_pass = new Date().getTime() - start_time; //an upper boundary of the time taken by the API to return response
                     console.log("time passed: " + time_pass );
                     //check if song is about to end, and play next song
-                    var limit = count/2;
+                    var limit = count/4;
                     if (nowPlaying.playing && nowPlaying.currentPosition === 0 && time_pass < limit) {
                         play(room); //this call must return before the next getMyCurrentPlaybackState request is sent
                     }
